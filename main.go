1eac8235a9b026be94176fd5ad0078b6fd94d359
intmian <1437131326@qq.com>
2019-09-17 20:27:38 +0800

    完成了简单的gui

--- main.go
+++ main.go
@@ -5,8 +5,39 @@
  */
 package main
 
-import "fileGenerate/tool"
+import (
+	"fileGenerate/tool"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	print(tool.SimpleFillTemp("hexo"))
+
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	myFolder := `template`
+	files, _ := ioutil.ReadDir(myFolder)
+	fileNames := make([]string, len(files))
+	for i, file := range files {
+		fileNames[i] = file.Name()
+	}
+	println("请选择模式1.低级 2.高级")
+	var mode int
+	_, _ = fmt.Scan(&mode)
+	switch mode {
+	case 1:
+		println("请选择模板")
+		for i, name := range fileNames {
+			fmt.Printf("%d %s\n", i, name)
+		}
+		var no int
+		_, _ = fmt.Scan(&no)
+		println("输入文件名")
+		var s string
+		_, _ = fmt.Scan(&s)
+		_ = ioutil.WriteFile(dir+`\`+s, []byte(tool.SimpleFillTemp(fileNames[no])), 0666) //写入文件(字节数组)
+	default:
+		println("无此模式")
+	}
 }
